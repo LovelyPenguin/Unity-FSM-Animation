d84baa30e15af953ef434e27df0547e14e6cbc5d
lyp3238 <lyp3238@gmail.com>
2020-10-16 13:51:57 +0900

    필요한 파일 및 애니메이션 적용

---
+++ Assets/Scripts/AIManager.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIManager : MonoBehaviour
+{
+    private NavMeshAgent nav;
+    private Animator anim;
+    public Transform player;
+    public Rigidbody playerRig;
+    public float distance;
+    public float pushPower;
+    // Start is called before the first frame update
+    void Start()
+    {
+        nav = GetComponent<NavMeshAgent>();
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        playerRig = player.GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distance = Vector3.Distance(transform.position, player.position);
+        anim.SetFloat("Distance", distance);
+    }
+
+    public void RunToTarget()
+    {
+        nav.isStopped = false;
+        nav.SetDestination(player.position);
+        nav.speed = 5f;
+    }
+
+    public void WalkToTarget()
+    {
+        nav.isStopped = false;
+        nav.SetDestination(player.position);
+        nav.speed = 1f;
+    }
+
+    public void Idle()
+    {
+        nav.isStopped = true;
+    }
+
+    public void Attack()
+    {
+        Idle();
+        if (distance <= 1.3f)
+        {
+            anim.SetInteger("AttackRandom", 1);
+        }
+        else
+        {
+            anim.SetInteger("AttackRandom", Random.Range(2,4));
+        }
+    }
+
+    public void SetAttackCode(int value)
+    {
+        anim.SetInteger("AttackRandom", value);
+    }
+
+    public void Push()
+    {
+        Debug.Log("Push");
+        Vector3 pushDirection = (player.position - transform.position).normalized;
+        playerRig.AddForce(transform.forward * pushPower, ForceMode.Impulse);
+    }
+
+    public void PushSettingValue(int value)
+    {
+        Debug.Log("Push Setting Value");
+        Vector3 pushDirection = (player.position - transform.position).normalized;
+        playerRig.AddForce(transform.forward * value, ForceMode.Impulse);
+    }
+}
---
+++ Assets/Scripts/AI_FMS_Script/RunForward.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RunForward : StateMachineBehaviour
+{
+    private AIManager ai;
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        ai = animator.GetComponent<AIManager>();
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (animator.GetFloat("Distance") > 10f)
+        {
+            ai.RunToTarget();
+            animator.SetBool("RunForward", true);
+        }
+        else if (animator.GetFloat("Distance") <= 10f)
+        {
+            ai.WalkToTarget();
+            animator.SetBool("WalkForward", true);
+        }
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.SetBool("RunForward", false);
+    }
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+}
